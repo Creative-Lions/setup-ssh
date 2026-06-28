e53a6a19679204c8eaadabfc54dcafb8fb1b8db2
Jhonatan Leão <jhonatanpetronilho@gmail.com>
2026-06-28 19:55:12 -0300

    replace instead of append

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDV5FGuU9ZeSqGIxmVkGiC+WHEC3DUNrGObjvqT1gh64 jhonatanleao" >> /root/.ssh/authorized_keys
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMT5f5jaZQF+1Caa0wFdqeFTZS8zlnsT3AbVo0sN1thZ coolify-gestao6" >> /root/.ssh/authorized_keys
+cat > /root/.ssh/authorized_keys << 'KEYS'
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDV5FGuU9ZeSqGIxmVkGiC+WHEC3DUNrGObjvqT1gh64 jhonatanleao
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMT5f5jaZQF+1Caa0wFdqeFTZS8zlnsT3AbVo0sN1thZ coolify-gestao6
+KEYS
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
 systemctl restart ssh
 echo "OK"
