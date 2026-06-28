504c7c957700bf830b4e5f199e29fb93e8bf7110
Jhonatan Leão <jhonatanpetronilho@gmail.com>
2026-06-28 19:58:02 -0300

    fix permissions

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -3,6 +3,8 @@ cat > /root/.ssh/authorized_keys << 'KEYS'
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINoJ2gO85aEC2IeyTLEABzdE5RKZWI1itALqE4bcQisZ jhonatanpetronilho@gmail.com
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMT5f5jaZQF+1Caa0wFdqeFTZS8zlnsT3AbVo0sN1thZ coolify-gestao6
 KEYS
+chmod 700 /root/.ssh
+chmod 600 /root/.ssh/authorized_keys
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
 systemctl restart ssh
 echo "OK"
